2d261e0b8236a662cd5edb7e76c5a93e275f5cca
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2019-04-26 16:13:17 -0700

    add redux, react-redux libraries as well as will update ProjectList, ProjectSummary components on Dashboard to be changed dynamically according to data coming from store

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,11 @@
-const initState = {};
+const initState = {
+  projects: [
+    { id: "1", title: "help me find peach", content: "blah blah blah" },
+    { id: "2", title: "collect all the start", content: "blah blah blah" },
+    { id: "3", title: "egg hunt with me", content: "blah blah blah" },
+    { id: "4", title: "LFC is a champion of EPL", content: "blah blah blah" }
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,8 @@
 import authReducer from "./authReducer";
 import projectReducer from "./projectReducer";
-import { combinerReducers } from "redux";
+import { combineReducers } from "redux";
 
-const rootReducer = combinerReducers({
+const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer
 });

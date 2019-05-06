94f0481d74549941757d043f2c40ce2d16f7ad8d
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2019-05-05 23:03:25 -0700

    add initial according to user names, user first name and last names to project details and project summary components as well as correct date

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,13 +2,16 @@ export const createProject = project => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to db
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
+
     firestore
       .collection("projects")
       .add({
         ...project,
-        authorFirstName: "John",
-        authorLastName: "Due",
-        authorId: 12345,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorId: authorId,
         createdAt: new Date()
       })
       .then(() => {

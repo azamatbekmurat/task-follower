89d7ee616790aa1ac6a2812be736a204ad70a0b8
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2019-05-07 20:49:14 -0700

    deploy project

---
+++ dist/precache-manifest.9165cc225b1fc8d0d270687b5b45342c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba89a5a69838acd30e102208e650d83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48c71abb265124746418",
+    "url": "/static/css/main.05961b2e.chunk.css"
+  },
+  {
+    "revision": "971d26ae64b5126cf5f5",
+    "url": "/static/js/2.6a8742f4.chunk.js"
+  },
+  {
+    "revision": "48c71abb265124746418",
+    "url": "/static/js/main.b9d93acd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
